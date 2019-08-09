abc9f1f832d7cf2a8582558c8e8d0f67e87a8345
leilacoquard <leilacoquard@gmail.com>
2019-08-09 16:35:34 +0200

    =navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="container">
+    <div class="header">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item">Accueil</a>
+            <a class="navbar-item">A propos</a>
+            <a class="navbar-item">Mon CV</a>
+            <a class="navbar-item">Mes projets</a>
+            <a class="navbar-item">Contact</a>
+          </div>
+        </div>
+      </nav>
+    </div>
     <div>
-      <logo />
       <h1 class="title">Leïla Coquard</h1>
       <h2 class="subtitle">Mon portfolio</h2>
     </div>
