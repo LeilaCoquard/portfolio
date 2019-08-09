ec3e57c731ac8416416b23582cff4b57777fe5a5
leilacoquard <leilacoquard@gmail.com>
2019-08-09 16:11:45 +0200

    =vider template

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,13 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">LeilaCoquard</h1>
+      <h1 class="title">Leïla Coquard</h1>
       <h2 class="subtitle">Mon portfolio</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
@@ -48,9 +37,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
