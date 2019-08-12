7bef4a0b7c64c68df5a3484184cd228749ad5eaa
leilacoquard <leilacoquard@gmail.com>
2019-08-12 12:34:18 +0200

    =section projet

--- pages/index.vue
+++ pages/index.vue
@@ -12,20 +12,47 @@
       </div>
     </nav>
     <section id="accueil">
-      <div id="sous-accueil">
-        <h1 class="title">Leïla Coquard</h1>
-        <h2 class="subtitle">Développeuse web Junior</h2>
-      </div>
+      <h1 class="title">Leïla Coquard</h1>
+      <h2 class="subtitle">Développeuse web Junior</h2>
     </section>
 
     <section id="apropos">
       <h2>A propos</h2>
+      <h3>Leïla Coquard, 31 ans, en reconversion professionnelle</h3>
+      <h4>Développeuse web Junior</h4>
+      <p>Après des études en statistiques, spécialisée en actuariat, j'ai intégré le milieu professionnel en tant qu'actuaire pendant 6 ans. Cette expérience a eu pour effet de me faire murrir, de développer mon sens du travail bien fait et d'apprivoiser les relations humaines en entreprise. Ces 6 ans fut entrecoupé d'un voyage d'un an autour du monde. C'est fou tout ce qu'on peut apprendre pendant un voyage, repenser complétement ses schemas de pensées. blabla...</p>
     </section>
     <section id="moncv">
       <h2>Mon CV</h2>
     </section>
     <section id="mesprojets">
       <h2>Mes projets</h2>
+
+      <div class="card">
+        <div class="card-image">
+          <figure class="image P6">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">Projet OC 6</p>
+              <p class="subtitle is-6">Jeu de plateau</p>
+            </div>
+          </div>
+
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris.
+            <a
+              href
+            >blabla</a>
+            <br />
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+      </div>
     </section>
     <section id="contact">
       <h2>Contact</h2>
@@ -90,9 +117,5 @@ h2 {
   background-repeat: no-repeat;
   background-size: cover;
   padding: 100px;
-}
-
-#sous-accueil {
-  opacity: 0.5;
 }
 </style>
